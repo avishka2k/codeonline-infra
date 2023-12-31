5264de7501338f6e4b9c8dd3c30e9f69badca1c4
Avishka Prabath <avishka2k@gmail.com>
2023-12-31 15:23:26 +0000

    update inventory

--- terraform/create_image/inventory.tf
+++ terraform/create_image/inventory.tf
@@ -1,6 +1,16 @@
-data "template_file" "host" {
+data "template_file" "inventory" {
   template = file("inventory.tpl")
   vars = {
     host_ip = "module.gcp_vm"
+  }
+}
+
+resource "null_resource" "update_inventory" {
+  triggers = {
+    template = data.template_file.inventory.rendered
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${data.template_file.inventory.rendered}' > inventory.ini"
   }
 }
